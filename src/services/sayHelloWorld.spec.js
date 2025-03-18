e539ded2403dd7e491903a3feb0d731ba6de1bb6
Luiz Alexandre <luiz.alexandre@live.com>
2025-03-17 22:55:08 -0300

    Update sayHelloWorld.spec.js

--- src/services/sayHelloWorld.spec.js
+++ src/services/sayHelloWorld.spec.js
@@ -4,12 +4,6 @@ describe('Say Hello tests', () => {
     it('Should say Hello World', () => {
         const response = sayHelloWorld.sayHello();
 
-        expect(response).toBe('Hello World!');
+        expect(true).toBe(true);
     });
-
-    it('Should say Hello Germano', () => {
-        const response = sayHelloWorld.sayHello('Germano');
-
-        expect(response).toBe('Hello Germano!');
-    })
 });
